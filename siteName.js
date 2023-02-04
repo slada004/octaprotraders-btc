5e5168c0354f55e8556702c39d2b6f323546ba3a
Emmanuel Worgu <emmanuelworgu@gmail.com>
2023-02-03 17:36:06 -0800

    fixed the sitename

--- siteName.js
+++ siteName.js
@@ -4,12 +4,29 @@
 
 const text = document.getElementsByClassName('sitename');
 const siteNumber = document.getElementById('sitenumber');
+let arr = [];
+arr = siteName.split('');
+let newarr
+let index
 // console.log(text);
+
+for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i])
+
+    if (arr[i] === '.') {
+        // console.log(i)
+
+        index = i;
+    }
+}
+
+newarr = arr.splice(0, index);
+let newName = newarr.join('').toUpperCase();
 
 for (let i = 0; i < text.length; i++) {
     // console.log(text[i])
 
-    text[i].textContent = siteName;
+    text[i].textContent = newName;
     
     // const element = array[index];
     
@@ -20,7 +37,7 @@ for (let i = 0; i < text.length; i++) {
         return null;
     }
 
-    if (siteName === 'elitecryptoinvestors.com') {
+    if (siteName === 'plantonicextrade.com') {
         siteNumber.textContent = '+1(479)485-1098'
     } else {
         return;
