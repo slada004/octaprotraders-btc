9e9a2c57b83355f5acd48f577866d9668ecf15e4
slada004 <124179661+slada004@users.noreply.github.com>
2025-04-04 07:14:50 -0400

    Update chat.js

--- chat.js
+++ chat.js
@@ -24,7 +24,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/64a81947cc26a871b026f855/1h4oa5h03';
+s1.src='https://code.jivosite.com/widget/8zZij8NeZm';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
